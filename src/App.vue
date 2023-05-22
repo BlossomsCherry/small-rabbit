84a41132d16c692899800da34772baf3fd9313e9
coder why <coderwhy@gmail.com>
2023-05-22 22:24:12 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,9 @@
+<template>
+    <div class="app">
+        <el-button type="primary">Primary</el-button>
+    </div>
+</template>
+
+<script setup></script>
+
+<style lang="less" scoped></style>
---
+++ src/services/request/index.js
@@ -0,0 +1,28 @@
+import axios from 'axios'
+
+const httpInstance = axios.create({
+    baseURL: 'http://pcapi-xiaotuxian-front-devtest.itheima.net',
+    timeout: 5000
+})
+
+//请求拦截器
+httpInstance.interceptors.request.use(
+    config => {
+        return config
+    },
+    err => {
+        return err
+    }
+)
+
+//响应拦截器
+httpInstance.interceptors.response.use(
+    config => {
+        return config
+    },
+    err => {
+        return err
+    }
+)
+
+export default httpInstance
